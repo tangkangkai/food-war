b33dc5d9a1a3e4304b82fd1e9ca6166932434c12
dqlkx <alex>
2014-10-07 14:16:22 -0700

    add a new scrollview for GameScene

--- demo.spritebuilder/Source/GameScene.m
+++ demo.spritebuilder/Source/GameScene.m
@@ -14,10 +14,8 @@ static GameScene* GameSceneInstance;
 
 @implementation GameScene {
     //CCTextField *_text;
-    NSMutableArray *levelArray;
-    BOOL ifLocked;
-    Selectscroll *ss;
-    CCNode *_gs_scroll;
+   // NSMutableArray *levelArray;
+ //   BOOL ifLocked;
     
 }
 
@@ -27,24 +25,18 @@ static GameScene* GameSceneInstance;
 
 -(id) init
 {
-    // always call "super" init
-    // Apple recommends to re-assign "self" with the "super" return value
     if( (self=[super init])) {
         GameSceneInstance=self;
-        
-        Selectscroll* sl=[[Selectscroll alloc] init];
-    //    sl.tag=10;
-        [self addChild:sl z:100];
-   //     self.isTouchEnabled=YES;
-        
+    //    Selectscroll* sl=[[Selectscroll alloc] init];
+     //   [self addChild:sl z:100];
     }
     return self;
 }
 
+/*
 - (void) didLoadFromCCB {
     NSLog(@"Enter Game Level Scene");
     self.userInteractionEnabled = TRUE;
- //   ss=[_gs_scroll children][0];
     if ([SavedData level]) {
         _text.string = [NSString stringWithFormat:@"Level %d", [SavedData level]];
     } else {
@@ -53,34 +45,21 @@ static GameScene* GameSceneInstance;
     levelArray = [SavedData levelArray];
 }
 
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
-    NSLog(@"mouse is moving");
-    
-}
-
+*/
 - (void) back {
     CCScene *mainScene = [CCBReader loadAsScene:@"MainScene"];
     CCTransition *trans = [CCTransition transitionPushWithDirection: CCTransitionDirectionRight duration:0.5f];
     [[CCDirector sharedDirector] replaceScene:mainScene withTransition:trans];
 }
 
+/*
 - (void) store {
     CCScene *storeScene = [CCBReader loadAsScene:@"StoreScene"];
     CCTransition *trans = [CCTransition transitionPushWithDirection: CCTransitionDirectionUp duration:0.5f];
     [[CCDirector sharedDirector] replaceScene:storeScene withTransition:trans];
 }
+*/
 
-- (void)next {
-    if (ifLocked) {
-        _text.string = @"Level Locked, please choose again";
-        return;
-    }
-    
-    CCScene *choiceScene = [CCBReader loadAsScene:@"ChoiceScene"];
-    
-    CCTransition *trans = [CCTransition transitionPushWithDirection:CCTransitionDirectionLeft duration:0.5f];
-    [[CCDirector sharedDirector] replaceScene:choiceScene withTransition:trans];
-}
 
 /*
 - (void)level1 {
@@ -104,25 +83,6 @@ static GameScene* GameSceneInstance;
     [self changeLevel:4];
 }
 */
-- (void)changeLevel: (int) level {
-        
-    if ([[levelArray objectAtIndex:level - 1] intValue] == 0) {
-        CCActionMoveTo *moveleft = [CCActionMoveTo actionWithDuration:0.05f position:ccp(0.4, 0.8)];
-        CCActionMoveTo *moveright = [CCActionMoveTo actionWithDuration:0.05f position:ccp(0.6, 0.8)];
-        CCActionMoveTo *moveback = [CCActionMoveTo actionWithDuration:0.05f position:ccp(0.5, 0.8)];
-        
-        CCActionSequence *sequence = [CCActionSequence actionWithArray:@[moveleft, moveright, moveleft, moveright, moveback]];
-        _text.string = [NSString stringWithFormat:@"Level %d locked", level];
-        [_text runAction:sequence];
-        ifLocked = true;
-    } else {
-        CCActionRotateBy *rotate = [CCActionRotateBy actionWithDuration:0.2f angle:360];
-        [_text runAction:rotate];
-        _text.string = [NSString stringWithFormat:@"Level %d", level];
-        [SavedData setLevel:level];
-        ifLocked = false;
-    }
 
-}
 
 @end
--- demo.spritebuilder/Source/Selectscroll.m
+++ demo.spritebuilder/Source/Selectscroll.m
@@ -8,27 +8,76 @@
 
 #import "Selectscroll.h"
 #import "GameScene.h"
+#import "SavedData.h"
 
-@implementation Selectscroll
+@implementation Selectscroll{
+    NSMutableArray *levelArray;
+    BOOL ifLocked;
+    GameScene *gs;
+}
+
+- (void) didLoadFromCCB {
+    NSLog(@"Enter Game Level Scene");
+    gs=[GameScene sharelayer];
+    self.userInteractionEnabled = TRUE;
+    if ([SavedData level]) {
+        gs.text.string = [NSString stringWithFormat:@"Level %d", [SavedData level]];
+    } else {
+        gs.text.string = @"Please choose your level";
+    }
+    levelArray = [SavedData levelArray];
+}
 
 
 - (void)level1 {
     NSLog(@"level1 button");
-    GameScene *a=[GameScene sharelayer];
-    a.text.string = @"Level Locked, please choose again";
-    NSLog(@"text is : %@",a.text);
+    [self changeLevel:1];
+    [self next];
     
 }
 
 - (void)level2 {
     NSLog(@"level2 button");
-    
+    [self changeLevel:2];
+    [self next];
 }
 
 - (void)level3 {
     NSLog(@"level3 button");
-    
-    
+    [self changeLevel:3];
+    [self next];
 }
 
+- (void)next {
+    if (ifLocked) {
+        gs.text.string = @"Level Locked, please choose again";
+        return;
+    }
+    
+    CCScene *choiceScene = [CCBReader loadAsScene:@"ChoiceScene"];
+    
+    CCTransition *trans = [CCTransition transitionPushWithDirection:CCTransitionDirectionLeft duration:0.5f];
+    [[CCDirector sharedDirector] replaceScene:choiceScene withTransition:trans];
+}
+
+- (void)changeLevel: (int) level {
+    gs=[GameScene sharelayer];
+    if ([[levelArray objectAtIndex:level - 1] intValue] == 0) {
+        CCActionMoveTo *moveleft = [CCActionMoveTo actionWithDuration:0.05f position:ccp(0.4, 0.8)];
+        CCActionMoveTo *moveright = [CCActionMoveTo actionWithDuration:0.05f position:ccp(0.6, 0.8)];
+        CCActionMoveTo *moveback = [CCActionMoveTo actionWithDuration:0.05f position:ccp(0.5, 0.8)];
+        
+        CCActionSequence *sequence = [CCActionSequence actionWithArray:@[moveleft, moveright, moveleft, moveright, moveback]];
+        gs.text.string = [NSString stringWithFormat:@"Level %d locked", level];
+        [gs.text runAction:sequence];
+        ifLocked = true;
+    } else {
+        CCActionRotateBy *rotate = [CCActionRotateBy actionWithDuration:0.2f angle:360];
+        [gs.text runAction:rotate];
+        gs.text.string  = [NSString stringWithFormat:@"Level %d", level];
+    //   [SavedData setLevel:level];
+        ifLocked = false;
+    }
+    
+}
 @end
