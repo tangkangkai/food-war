2aac515456f4b4411141be73342017d4629bae0c
dqlkx <alex>
2014-10-07 15:24:13 -0700

    update the image

--- demo.spritebuilder/Source/GameScene.m
+++ demo.spritebuilder/Source/GameScene.m
@@ -7,16 +7,11 @@
 //
 
 #import "GameScene.h"
-#import "SavedData.h"
-#import "Selectscroll.h"
 
 static GameScene* GameSceneInstance;
 
 @implementation GameScene {
-   //CCTextField *_text;
-   // NSMutableArray *levelArray;
-   // BOOL ifLocked;
-    
+
 }
 
 +(GameScene*)sharelayer{
@@ -27,62 +22,16 @@ static GameScene* GameSceneInstance;
 {
     if( (self=[super init])) {
         GameSceneInstance=self;
-    //    Selectscroll* sl=[[Selectscroll alloc] init];
-     //   [self addChild:sl z:100];
     }
     return self;
 }
 
-/*
-- (void) didLoadFromCCB {
-    NSLog(@"Enter Game Level Scene");
-    self.userInteractionEnabled = TRUE;
-    if ([SavedData level]) {
-        _text.string = [NSString stringWithFormat:@"Level %d", [SavedData level]];
-    } else {
-        _text.string = @"Please choose your level";
-    }
-    levelArray = [SavedData levelArray];
-}
-
-*/
 - (void) back {
     CCScene *mainScene = [CCBReader loadAsScene:@"MainScene"];
     CCTransition *trans = [CCTransition transitionPushWithDirection: CCTransitionDirectionRight duration:0.5f];
     [[CCDirector sharedDirector] replaceScene:mainScene withTransition:trans];
 }
 
-/*
-- (void) store {
-    CCScene *storeScene = [CCBReader loadAsScene:@"StoreScene"];
-    CCTransition *trans = [CCTransition transitionPushWithDirection: CCTransitionDirectionUp duration:0.5f];
-    [[CCDirector sharedDirector] replaceScene:storeScene withTransition:trans];
-}
-*/
-
-
-/*
-- (void)level1 {
-    NSLog(@"level1 button");
-    [self changeLevel:1];
-}
-
-- (void)level2 {
-    NSLog(@"level2 button");
-    [self changeLevel:2];
-}
-
-- (void)level3 {
-    NSLog(@"level3 button");
-    [self changeLevel:3];
-    
-}
-
-- (void)level4 {
-    NSLog(@"level4 button");
-    [self changeLevel:4];
-}
-*/
 
 
 @end
