f78a62c367bba7b5e1736a4ce871fa99c69e827d
dqlkx <alex>
2014-10-07 21:01:07 -0700

    change sprite builder

---
+++ demo.spritebuilder/Source/Selectroll.m
@@ -0,0 +1,14 @@
+//
+//  Selectroll.m
+//  demo
+//
+//  Created by dqlkx on 10/7/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import "Selectroll.h"
+
+
+@implementation Selectroll
+
+@end
