dc1c45f20ce87385668ec1a9ce346a1bfd309c21
dqlkx <alex>
2014-10-12 13:18:10 -0700

    based launch

--- demo.spritebuilder/Source/Scrollback.m
+++ demo.spritebuilder/Source/Scrollback.m
@@ -10,8 +10,10 @@
 #import "Soldier.h"
 
 @implementation Scrollback{
-    
-
+    CCNode *_missile;
+    long _healtharraysize;
+    long _junkarraysize;
+    int _startlaunch;
 
 }
 
@@ -28,7 +30,67 @@
 - (void)didLoadFromCCB {
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
-    [self schedule:@selector(enemy_autobuild:) interval:6];
+    [self schedule:@selector(enemy_autobuild:) interval:3];
+    [self schedule:@selector(detect) interval:0.2];
+}
+
+-(void)detect{
+    _healtharraysize = [_healthy_soldiers count];
+    _junkarraysize = [_junk_soldiers count];
+    if (_junkarraysize!=0) {
+        for( long i = 0; i < _healtharraysize; i++ ){
+            //    NSLog(@"x:%f, y:%f ",s.position.x,s.position.y);
+            [[[_healthy_soldiers objectAtIndex:i] soldier] stopAllActions];
+
+        }
+    }
+
+}
+
+-(void)missileLaunch:(CCNode *)missile :(CGPoint ) touchLocation{
+    CCActionRotateBy *rotate = [CCActionRotateBy actionWithDuration:1.0f angle:90.f];
+    CCActionJumpTo* jumpUp = [CCActionJumpTo actionWithDuration:1.0f position:touchLocation
+                                                         height:touchLocation.y jumps:1];
+    CCActionSpawn *groupAction = [CCActionSpawn actionWithArray:@[rotate, jumpUp]];
+    CCActionSequence *sequence = [CCActionSequence actionWithArray:@[groupAction, [CCActionCallFunc actionWithTarget:self selector:@selector(missileRemoved)]]];
+    // allDone is your method to run...
+    [missile runAction:sequence];
+
+}
+
+- (void)missileRemoved
+{
+    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"explosion"];
+    //  make the particle effect clean itself up, once it is completed
+    explosion.autoRemoveOnFinish = TRUE;
+    
+    explosion.duration = 5;
+    
+    // place the particle effect on the seals position
+    explosion.position = _missile.position;
+    // add the particle effect to the same node the seal is on
+    [_missile.parent addChild:explosion];
+    
+    // finally, remove the destroyed seal
+    [_missile removeFromParent];
+}
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    NSLog(@"Scrollback touch began");
+    CGPoint touchLocation = [touch locationInNode:self];
+    if (_startlaunch==1) {
+        [self missileLaunch:_missile :touchLocation];
+    }
+    for( long i = 0; i < _healtharraysize; i++ ){
+        if(CGRectContainsPoint([[[_healthy_soldiers objectAtIndex:i] soldier] boundingBox],touchLocation))
+        {
+            NSLog(@"touch healthy food");
+            _missile = [CCBReader load:@"missle"];
+            _missile.position=[[_healthy_soldiers objectAtIndex:i] soldier].position;
+            [self addChild:_missile];
+            _startlaunch=1;
+        }    
+    }
 }
 
 
