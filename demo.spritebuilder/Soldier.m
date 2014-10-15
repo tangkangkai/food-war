c9225059b4263b347adaa7934eec08de8c7c44c8
Shuoqing Ding <dsq704136@gmail.com>
2014-10-14 20:07:51 -0700

    fix missile bug

--- demo.spritebuilder/Soldier.m
+++ demo.spritebuilder/Soldier.m
@@ -57,6 +57,15 @@
     return health;
 }
 
+- (NSMutableArray*)getArray:(int) enemy{
+    if( enemy )
+        return _enemyArray;
+    else{
+        return _ourArray;
+    }
+}
+
+
 - (CCNode*)getSoldier{
     return _soldier;
 }
@@ -144,7 +153,8 @@
     moving = false;
     last_attack_time = NULL;
     _lane_num = lane_num;
-    
+    audio = [OALSimpleAudio sharedInstance];
+
     if (img != NULL) {
         _soldier = [CCBReader load:img];
     }
@@ -482,14 +492,79 @@
     }
 }
 
-- (void) Launch{
+- (void) Launch:(CGPoint) targetLoc{
+    CCParticleSystem *fire = (CCParticleSystem *)[CCBReader load:@"fire"];
+    fire.autoRemoveOnFinish=true;
+    fire.duration=0.2;
+    CCActionRotateBy *rotate = [CCActionRotateBy actionWithDuration:1.0f angle:90.f];
+    CCActionJumpTo* jumpUp = [CCActionJumpTo actionWithDuration:1.0f position:targetLoc
+                                                         height:80 jumps:1];
+    CCActionSpawn *groupAction = [CCActionSpawn actionWithArray:@[rotate, jumpUp]];
+    
+    
+    CCActionSequence *sequence = [CCActionSequence actionWithArray:@[groupAction, [CCActionCallFunc actionWithTarget:self selector:@selector(missileRemoved)]]];
+    // allDone is your method to run...
+    _missile = [CCBReader load:@"missle"];
+    _missile.position = [[ self getSoldier] position];
+    CCNode *parent = [[ self getSoldier ] parent];
+    [parent addChild:_missile];
+
+    fire.position=_missile.position;
+    [self addChild:fire];
+    [audio playEffect:@"missle_launch.mp3"];
+    [_missile runAction:sequence];
+
     last_attack_time = [NSDate date];
     [self undoReady];
+
+}
+
+- (void)missileRemoved
+{
+    NSMutableArray *_targetLoseHealth;
+    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"explosion"];
+    //  make the particle effect clean itself up, once it is completed
+    explosion.autoRemoveOnFinish = TRUE;
+    explosion.duration = 1;
+    
+    // place the particle effect on the seals position
+    explosion.position = _missile.position;
+    // add the particle effect to the same node the seal is on
+    [_missile.parent addChild:explosion];
+    [audio playEffect:@"explode.mp3"];
+    
+    _targetLoseHealth=[self missileDetect];
+    for (Soldier *target in _targetLoseHealth) {
+        [target loseHealth:atkPower];
+    }
+    // finally, remove the destroyed seal
+    [_missile removeFromParent];
+}
+
+//find target the missle hit
+-(NSMutableArray*)missileDetect{
+    //  int nearest_distance=[self missile_atk_range];
+    Soldier *soldier = NULL;
+    NSMutableArray* targets = [NSMutableArray arrayWithObjects:nil ];
+    int dx;
+    int dy;
+    int exploreRange = 40;
+    NSMutableArray *enemyArray = [ self getArray:1 ];
+    for( long i = 0; i < enemyArray.count; i++ ){
+        soldier=[enemyArray objectAtIndex:i];
+        dx=ABS(_missile.position.x-[[soldier soldier]position].x);
+        dy=ABS(_missile.position.y-[[soldier soldier] position].y);
+        double dist = sqrt(dx*dx + dy*dy);
+        if (dist < exploreRange) {
+            [targets addObject:[enemyArray objectAtIndex:i]];
+        }
+    }
+    return targets;
+    
 }
 
 -(void)move{
     if( !_readyLaunch ){
-        
         [super move];
     }
 }
