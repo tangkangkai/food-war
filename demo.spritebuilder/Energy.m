0272b33b43588d1f76761fbd278dc7365a62579c
dqlkx <dqlkx@dqlkxs-MacBook-Pro.local>
2014-11-15 23:39:22 -0800

    update deadbody position

--- demo.spritebuilder/Energy.m
+++ demo.spritebuilder/Energy.m
@@ -12,6 +12,7 @@
 
 @implementation Energy{
     CGPoint _arrivePosition;
+    NSDate *last_attack_time;
 }
 
 
@@ -19,11 +20,12 @@
 -(id)initEnergy:(int) value pos:(CGPoint) position bgNode:(CCNode*)bgNode
 {
     _deadBody=[CCBReader load:@"energy"];
-    position.y=position.y-10;
+    position.y=position.y-30;
     _deadBody.position=position;
     [bgNode addChild:_deadBody];
     _engergyValue=value;
-    [self schedule:@selector(arrive) interval:0.1f];
+  //  [self schedule:@selector(disappear) interval:0.1f];
+
     return self;
 }
 
@@ -33,7 +35,6 @@
 
 -(void) collect:(CCNode*)Icon Gameplay:(CCScrollView*)c{
     _arrivePosition=CGPointMake(Icon.position.x+[c scrollPosition].x, Icon.position.y);
-   // CCActionMoveTo *collectEnergy = [CCActionMoveTo actionWithDuration:1.0f position:_arrivePosition];
     CCActionRotateBy *actionRotate = [CCActionRotateBy actionWithDuration: 0.5f angle:360];
     CCActionJumpTo* jumpUp = [CCActionJumpTo actionWithDuration:1.0f position:_arrivePosition height:40 jumps:2];
     CCActionSpawn *groupAction = [CCActionSpawn actionWithArray:@[jumpUp,actionRotate]];
@@ -42,8 +43,14 @@
 }
 
 -(void) arrive{
-        [self unschedule:@selector(arrive)];
         [_deadBody removeFromParent];
         [Gameplay addEnergy:[self engergyValue]];
+}
+
+-(void) disappear{
+    if( last_attack_time == nil || [ last_attack_time timeIntervalSinceNow ]*-1 >= 2 ){
+        last_attack_time = [NSDate date];
+        [self removeFromParent];
+    }
 }
 @end
