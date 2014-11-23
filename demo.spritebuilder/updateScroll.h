72c8d7841bc8e94089c54fa75a56e22a5610eebc
dqlkx <dqlkx@guest-wireless-nup-nat-206-117-89-016.usc.edu>
2014-11-22 21:37:31 -0800

    for changing upgrade scence

---
+++ demo.spritebuilder/updateScroll.h
@@ -0,0 +1,16 @@
+//
+//  updateScroll.h
+//  demo
+//
+//  Created by dqlkx on 11/22/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface updateScroll : CCNode {
+    
+}
+
+@end
---
+++ demo.spritebuilder/updateScroll.m
@@ -0,0 +1,14 @@
+//
+//  updateScroll.m
+//  demo
+//
+//  Created by dqlkx on 11/22/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import "updateScroll.h"
+
+
+@implementation updateScroll
+
+@end
