07edaa5d22f199d2bc79da75bf77b270ee73c6dc
Shuoqing Ding <dsq704136@gmail.com>
2014-11-27 15:07:40 -0800

    The upgrade scence is under construction

--- demo.spritebuilder/updateScroll.m
+++ demo.spritebuilder/updateScroll.m
@@ -133,73 +133,74 @@
 
 
 - (void)updateShowedData {
-    NSMutableDictionary *soldierLevelDict = [SavedData soldierLevel];
-    //get level of soldiers
-    potatoLevel = [[soldierLevelDict objectForKey:@"potato"] intValue];
-    beanLevel = [[soldierLevelDict objectForKey:@"bean"] intValue];
-    bananaLevel = [[soldierLevelDict objectForKey:@"banana"] intValue];
-    cornLevel = [[soldierLevelDict objectForKey:@"corn"] intValue];
-    
-    //set the level
-    _level1.string = [NSString stringWithFormat:@"Lv.%d", potatoLevel];
-    _level2.string = [NSString stringWithFormat:@"Lv.%d", beanLevel];
-    _level3.string = [NSString stringWithFormat:@"Lv.%d", bananaLevel];
-    _level4.string = [NSString stringWithFormat:@"Lv.%d", cornLevel];
-    
-    // calculate the cost of each soldier if upgrading
-    beanCost = 200 * beanLevel;
-    potatoCost = 150 * potatoLevel;
-    bananaCost = 350 * bananaLevel;
-    cornCost = 300 * cornLevel;
-    
-    //set the price
-    _cost1.string = [NSString stringWithFormat:@"$ %d", potatoCost];
-    _cost2.string = [NSString stringWithFormat:@"$ %d", beanCost];
-    _cost3.string = [NSString stringWithFormat:@"$ %d", bananaCost];
-    _cost4.string = [NSString stringWithFormat:@"$ %d", cornCost];
-    
-    // value of health, atkPower, defense
-    int ph = [potato getHealth] / 5;
-    int pa = [potato getAtkPower];
-    int pd = [potato getDefence] * 200;
-    int beh = [bean getHealth] / 5;
-    int bea = [bean getAtkPower];
-    int bed = [bean getDefence] * 200;
-    int bah = [banana getHealth] / 5;
-    int baa = [banana getAtkPower];
-    int bad = [banana getDefence] * 200;
-    int ch = [corn getHealth] / 5;
-    int ca = [corn getAtkPower];
-    int cd = [corn getDefence] * 200;
-    
-    _potatoH.string = [NSString stringWithFormat:@"HP:%d", ph];
-    _potatoAtk.string = [NSString stringWithFormat:@"Atk:%d", pa];
-    _potatoD.string = [NSString stringWithFormat:@"D:%d", pd];
-    _potatoHealth.contentSize = CGSizeMake(ph, _potatoHealth.contentSize.height);
-    _potatoAtkPower.contentSize = CGSizeMake(pa, _potatoAtkPower.contentSize.height);
-    _potatoDefense.contentSize = CGSizeMake(pd, _potatoDefense.contentSize.height);
-    
-    
-    _beanH.string = [NSString stringWithFormat:@"HP:%d", beh];
-    _beanAtk.string = [NSString stringWithFormat:@"Atk:%d", bea];
-    _beanD.string = [NSString stringWithFormat:@"D:%d", bed];
-    _beanHealth.contentSize = CGSizeMake(beh, _beanHealth.contentSize.height);
-    _beanAtkPower.contentSize = CGSizeMake(bea, _beanAtkPower.contentSize.height);
-    _beanDefense.contentSize = CGSizeMake(bed, _beanDefense.contentSize.height);
-    
-    _bananaH.string = [NSString stringWithFormat:@"HP:%d", bah];
-    _bananaAtk.string = [NSString stringWithFormat:@"Atk:%d", baa];
-    _bananaD.string = [NSString stringWithFormat:@"D:%d", bad];
-    _bananaHealth.contentSize = CGSizeMake(bah, _bananaHealth.contentSize.height);
-    _bananaAtkPower.contentSize = CGSizeMake(baa, _bananaAtkPower.contentSize.height);
-    _bananaDefense.contentSize = CGSizeMake(bad, _bananaDefense.contentSize.height);
-    
-    _cornH.string = [NSString stringWithFormat:@"HP:%d", ch];
-    _cornAtk.string = [NSString stringWithFormat:@"Atk:%d", ca];
-    _cornD.string = [NSString stringWithFormat:@"D:%d", cd];
-    _cornHealth.contentSize = CGSizeMake(ch / 2, _cornHealth.contentSize.height);
-    _cornAtkPower.contentSize = CGSizeMake(ca / 2, _cornAtkPower.contentSize.height);
-    _cornDefense.contentSize = CGSizeMake(cd / 2, _cornDefense.contentSize.height);
+    return;
+//    NSMutableDictionary *soldierLevelDict = [SavedData soldierLevel];
+//    //get level of soldiers
+//    potatoLevel = [[soldierLevelDict objectForKey:@"potato"] intValue];
+//    beanLevel = [[soldierLevelDict objectForKey:@"bean"] intValue];
+//    bananaLevel = [[soldierLevelDict objectForKey:@"banana"] intValue];
+//    cornLevel = [[soldierLevelDict objectForKey:@"corn"] intValue];
+//    
+//    //set the level
+//    _level1.string = [NSString stringWithFormat:@"Lv.%d", potatoLevel];
+//    _level2.string = [NSString stringWithFormat:@"Lv.%d", beanLevel];
+//    _level3.string = [NSString stringWithFormat:@"Lv.%d", bananaLevel];
+//    _level4.string = [NSString stringWithFormat:@"Lv.%d", cornLevel];
+//    
+//    // calculate the cost of each soldier if upgrading
+//    beanCost = 200 * beanLevel;
+//    potatoCost = 150 * potatoLevel;
+//    bananaCost = 350 * bananaLevel;
+//    cornCost = 300 * cornLevel;
+//    
+//    //set the price
+//    _cost1.string = [NSString stringWithFormat:@"$ %d", potatoCost];
+//    _cost2.string = [NSString stringWithFormat:@"$ %d", beanCost];
+//    _cost3.string = [NSString stringWithFormat:@"$ %d", bananaCost];
+//    _cost4.string = [NSString stringWithFormat:@"$ %d", cornCost];
+//    
+//    // value of health, atkPower, defense
+//    int ph = [potato getHealth] / 5;
+//    int pa = [potato getAtkPower];
+//    int pd = [potato getDefence] * 200;
+//    int beh = [bean getHealth] / 5;
+//    int bea = [bean getAtkPower];
+//    int bed = [bean getDefence] * 200;
+//    int bah = [banana getHealth] / 5;
+//    int baa = [banana getAtkPower];
+//    int bad = [banana getDefence] * 200;
+//    int ch = [corn getHealth] / 5;
+//    int ca = [corn getAtkPower];
+//    int cd = [corn getDefence] * 200;
+//    
+//    _potatoH.string = [NSString stringWithFormat:@"HP:%d", ph];
+//    _potatoAtk.string = [NSString stringWithFormat:@"Atk:%d", pa];
+//    _potatoD.string = [NSString stringWithFormat:@"D:%d", pd];
+//    _potatoHealth.contentSize = CGSizeMake(ph, _potatoHealth.contentSize.height);
+//    _potatoAtkPower.contentSize = CGSizeMake(pa, _potatoAtkPower.contentSize.height);
+//    _potatoDefense.contentSize = CGSizeMake(pd, _potatoDefense.contentSize.height);
+//    
+//    
+//    _beanH.string = [NSString stringWithFormat:@"HP:%d", beh];
+//    _beanAtk.string = [NSString stringWithFormat:@"Atk:%d", bea];
+//    _beanD.string = [NSString stringWithFormat:@"D:%d", bed];
+//    _beanHealth.contentSize = CGSizeMake(beh, _beanHealth.contentSize.height);
+//    _beanAtkPower.contentSize = CGSizeMake(bea, _beanAtkPower.contentSize.height);
+//    _beanDefense.contentSize = CGSizeMake(bed, _beanDefense.contentSize.height);
+//    
+//    _bananaH.string = [NSString stringWithFormat:@"HP:%d", bah];
+//    _bananaAtk.string = [NSString stringWithFormat:@"Atk:%d", baa];
+//    _bananaD.string = [NSString stringWithFormat:@"D:%d", bad];
+//    _bananaHealth.contentSize = CGSizeMake(bah, _bananaHealth.contentSize.height);
+//    _bananaAtkPower.contentSize = CGSizeMake(baa, _bananaAtkPower.contentSize.height);
+//    _bananaDefense.contentSize = CGSizeMake(bad, _bananaDefense.contentSize.height);
+//    
+//    _cornH.string = [NSString stringWithFormat:@"HP:%d", ch];
+//    _cornAtk.string = [NSString stringWithFormat:@"Atk:%d", ca];
+//    _cornD.string = [NSString stringWithFormat:@"D:%d", cd];
+//    _cornHealth.contentSize = CGSizeMake(ch / 2, _cornHealth.contentSize.height);
+//    _cornAtkPower.contentSize = CGSizeMake(ca / 2, _cornAtkPower.contentSize.height);
+//    _cornDefense.contentSize = CGSizeMake(cd / 2, _cornDefense.contentSize.height);
     
 }
 
