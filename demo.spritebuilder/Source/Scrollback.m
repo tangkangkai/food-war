b1504763d51e67f0e986c4a17cc4374d8649e0e4
Shuoqing Ding <dsq704136@gmail.com>
2014-11-30 00:08:06 -0800

    Fix foodtruck bug

--- demo.spritebuilder/Source/Scrollback.m
+++ demo.spritebuilder/Source/Scrollback.m
@@ -234,9 +234,9 @@ const int ICEBUCKET = 2;
     if( num == 3 )
         [_track3 setVisible:true];
     
-    for (Soldier* junk in _junk_soldiers) {
-        [[junk getSoldier] setZOrder:2000];
-    }
+//    for (Soldier* junk in _junk_soldiers) {
+//        [[junk getSoldier] setZOrder:2000];
+//    }
 }
 
 + (void) fillEnergyArray:(CCNode*)energy{
